d204a4cc610896e873cd0cd5c325ba4031086e72
Myroslava Shulimenko <mira.shulimenko@gmail.com>
2023-08-08 12:38:21 +0200

    test start

---
+++ JH_0727/src/test/java/org/example/HandlerTest.java
@@ -0,0 +1,23 @@
+package org.example;
+
+
+import org.junit.jupiter.api.Test;
+
+class HandlerTest {
+
+    @Test
+    void findSportCarMan() {
+    }
+
+    @Test
+    void gpoupGender() {
+    }
+
+    @Test
+    void maxSpeed() {
+    }
+
+    @Test
+    void writer() {
+    }
+}
