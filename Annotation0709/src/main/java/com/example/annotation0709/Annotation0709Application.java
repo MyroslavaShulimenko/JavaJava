6bd750dcfe8005409e4c410f09f5c130e542389d
Myroslava Shulimenko <mira.shulimenko@gmail.com>
2023-09-07 22:45:42 +0200

    0709

---
+++ Annotation0709/src/main/java/com/example/annotation0709/Annotation0709Application.java
@@ -0,0 +1,13 @@
+package com.example.annotation0709;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Annotation0709Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Annotation0709Application.class, args);
+	}
+
+}
---
+++ Annotation0709/src/test/java/com/example/annotation0709/Annotation0709ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.annotation0709;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Annotation0709ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
